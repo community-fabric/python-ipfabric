8f98cbf8687802056bbf89076d88440c2c6c582c
Justin Jeffery <justin.jeffery@ipfabric.io>
2021-12-01 17:56:52 -0500

    feat: cve check tool

---
+++ ipfabric/tools/vulnerabilities.py
@@ -0,0 +1,53 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+from ipfabric.tools.nist import NIST, CVEs
+
+
+class Version(BaseModel):
+    vendor: str
+    family: Optional[str]
+    version: str
+    cves: CVEs
+    hostname: Optional[str]
+    site: Optional[str]
+
+
+class Vulnerabilities:
+    def __init__(self, ipf):
+        self.ipf = ipf
+        self.nist = NIST(base_url='https://services.nvd.nist.gov/rest/json/cves/1.0', timeout=30)
+
+    def __del__(self):
+        try:
+            self.nist.close()
+        except AttributeError:
+            pass
+
+    def _check_versions(self, versions):
+        cves = list()
+        for v in versions:
+            cve = self.nist.check_cve(v['vendor'], v['family'], v['version'])
+            version = Version(vendor=v['vendor'], family=v['family'], version=v['version'], cves=cve)
+            if 'hostname' in v:
+                version.hostname = v['hostname']
+                version.site = v['siteName']
+            cves.append(version)
+        return cves
+
+    def check_versions(self, vendor=None):
+        filters = {'vendor': ['like', vendor]} if vendor else None
+        versions = self.ipf.fetch_all('tables/management/osver-consistency', columns=['vendor', 'family', 'version'],
+                                      filters=filters)
+        return self._check_versions(versions)
+
+    def check_device(self, device):
+        devices = self.ipf.inventory.devices.all(columns=['hostname', 'siteName', 'vendor', 'family', 'version'],
+                                                 filters={'hostname': ['like', device]})
+        return self._check_versions(devices)
+
+    def check_site(self, site):
+        sites = self.ipf.inventory.devices.all(columns=['hostname', 'siteName', 'vendor', 'family', 'version'],
+                                               filters={'siteName': ['like', site]})
+        return self._check_versions(sites)
