a54c675f0625716061e23535b675664956bd4a88
Justin Jeffery <justin.jeffery@ipfabric.io>
2021-12-02 15:52:48 -0500

    feat: Added seeds with examples.

---
+++ examples/seeds.py
@@ -0,0 +1,38 @@
+"""
+seeds.py
+"""
+from ipfabric import IPFClient
+from ipfabric.settings import Seeds
+
+
+if __name__ == '__main__':
+    ipf = IPFClient('https://demo3.ipfabric.io/')  # Token must have Setting Permissions
+    ipf_seeds = Seeds(client=ipf)
+
+    print(ipf_seeds.seeds)
+    """
+    ['10.64.128.3', '10.64.128.1', '10.66.255.105', '10.67.255.108', '10.67.255.107']
+    """
+    print()
+
+    add = ipf_seeds.add_seeds('127.0.0.1')  # Can be a single IP or list
+    print(add)
+    """
+    ['10.64.128.3', '10.64.128.1', '10.66.255.105', '127.0.0.1', '10.67.255.107', '10.67.255.108']
+    """
+    print()
+
+    delete = ipf_seeds.delete_seeds('127.0.0.1')  # Can be a single IP or list
+    print(delete)
+    """
+    ['10.66.255.105', '10.64.128.3', '10.67.255.108', '10.64.128.1', '10.67.255.107']
+    """
+    print()
+
+    update = ipf_seeds.set_seeds(["10.64.128.3", "10.64.128.1", "10.66.255.105", "10.67.255.108", "10.67.255.107"])
+    # Will replace the entire configuration with this new list
+    print(update)
+    """
+    ['10.64.128.3', '10.64.128.1', '10.66.255.105', '10.67.255.108', '10.67.255.107']
+    """
+    print()
---
+++ ipfabric/settings/seeds.py
@@ -0,0 +1,64 @@
+import logging
+from ipaddress import IPv4Interface, AddressValueError
+from typing import Any
+
+from pydantic.dataclasses import dataclass
+
+logger = logging.getLogger()
+
+
+@dataclass
+class Seeds:
+    client: Any
+
+    @property
+    def seeds(self):
+        res = self.client.get('settings/seed')
+        res.raise_for_status()
+        return res.json()
+
+    @staticmethod
+    def _check_seeds(seeds):
+        valid = True
+        for seed in seeds:
+            try:
+                IPv4Interface(seed)
+            except AddressValueError:
+                valid = False
+                logger.error(f"Seed {seed} is not a valid IP address or network.")
+        return valid
+
+    def set_seeds(self, seeds):
+        """
+        Sets the seeds with supplied list, will override current configuration.
+        :param seeds: list: List of IP addresses or networks
+        :return: list: Updated list of configured seeds
+        """
+        if not isinstance(seeds, list):
+            raise SyntaxError('Seeds must be a list of IP Addresses or networks')
+        if self._check_seeds(seeds):
+            res = self.client.put('settings/seed', json=seeds)
+            res.raise_for_status()
+            return res.json()
+
+    def add_seeds(self, seeds):
+        """
+        Adds a single seed or list of seeds to the current configuration
+        :param seeds: list: List of IP addresses or networks
+        :return: list: Updated list of configured seeds
+        """
+        if isinstance(seeds, str):
+            seeds = [seeds]
+        updated = list(set.union(set(self.seeds), set(seeds)))
+        return self.set_seeds(updated)
+
+    def delete_seeds(self, seeds):
+        """
+        Deletes a seed or a list of seeds from what is configured.
+        :param seeds: list: List of IP addresses or networks
+        :return: list: Updated list of configured seeds
+        """
+        if isinstance(seeds, str):
+            seeds = [seeds]
+        updated = list(set(self.seeds) - set(seeds))
+        return self.set_seeds(updated)
