7777dc867398540cf9274badcd82129bbfb4b94c
Justin Jeffery <justin.jeffery@ipfabric.io>
2021-12-08 11:15:56 -0500

    fix: Fix for usermgmt test: added tests and examples

---
+++ examples/settings/user_management.py
@@ -0,0 +1,49 @@
+"""
+user_management.py
+"""
+from ipfabric import IPFClient
+from ipfabric.settings import UserMgmt
+
+
+if __name__ == '__main__':
+    ipf = IPFClient('https://demo3.ipfabric.io/')  # Token must have User Management Permissions
+    usermgmt = UserMgmt(client=ipf)
+
+    print(usermgmt.users[0])
+    """
+    username='justin' scope=['read', 'write', 'settings'] email='justin.jeffrey@ipfabric.io' user_id='1108612054' 
+    local=True sso_provider=None domains='' custom_scope=True ldap_id=None
+    """
+    print()
+
+    user = usermgmt.add_user(username='Test', email='test@ipfabric.io', password='8characters',
+                             scope=['read', 'write', 'settings', 'team'])
+    print(user)
+    """
+    username='Test' scope=['read', 'write', 'settings', 'team'] email='test@ipfabric.io' user_id='1168572704' 
+    local=None sso_provider=None domains=None custom_scope=True ldap_id=None
+    """
+    print()
+
+    print(usermgmt.get_user_by_id(user_id=user.user_id))
+    """
+    username='Test' scope=['read', 'write', 'settings', 'team'] email='test@ipfabric.io' user_id='1168572704' 
+    local=None sso_provider=None domains=None custom_scope=True ldap_id=None
+    """
+    print()
+
+    print(usermgmt.get_users(username=user.username))
+    """
+    [User(username='Test', scope=['read', 'write', 'settings', 'team'], email='test@ipfabric.io', 
+    user_id='1168572704', local=True, sso_provider=None, domains='', custom_scope=True, ldap_id=None)]
+    """
+    print()
+
+    print(usermgmt.delete_user(user_id=user.user_id))
+    """
+    [User(username='justin', scope=['read', 'write', 'settings'], email='justin.jeffrey@ipfabric.io', 
+        user_id='1108612054', local=True, sso_provider=None, domains='', custom_scope=True, ldap_id=None), 
+    User(username='vector', scope=['read', 'write', 'settings'], email='vector@vector.pl', user_id='1083776225', 
+        local=True, sso_provider=None, domains='', custom_scope=True, ldap_id=None), ]
+    """
+    print()
--- tests/settings/test_users.py
+++ tests/settings/test_users.py
@@ -1,33 +1,45 @@
 import unittest
 from unittest.mock import MagicMock
 
-from ipfabric.settings import users
+from ipfabric.settings import user_mgmt
 
 
 class TestUsers(unittest.TestCase):
+    def setUp(self) -> None:
+        mock = MagicMock()
+        mock._ipf_pager.return_value = [{'id': '1108612054', 'isLocal': True, 'username': 'justin',
+                                         'ssoProvider': None, 'domainSuffixes': '',
+                                         'email': 'justin.jeffrey@ipfabric.io', 'customScope': True,
+                                         'scope': ['read', 'write', 'settings']}]
+        mock.get().json.return_value = {'id': '1108612054', 'isLocal': True, 'username': 'justin',
+                                         'ssoProvider': None, 'domainSuffixes': '',
+                                         'email': 'justin.jeffrey@ipfabric.io', 'customScope': True,
+                                         'scope': ['read', 'write', 'settings']}
+        self.usermgmt = user_mgmt.UserMgmt(mock)
+
     def test_users(self):
-        user = users.Users(MagicMock())
-        user.client._ipf_pager.return_value = [{'id': '1108612054', 'isLocal': True, 'username': 'justin',
-                                               'ssoProvider': None, 'domainSuffixes': '',
-                                               'email': 'justin.jeffrey@ipfabric.io', 'customScope': True,
-                                               'scope': ['read', 'write', 'settings']}]
-        u = user.users[0]
-        self.assertIsInstance(u, users.User)
+        u = self.usermgmt.users[0]
+        self.assertIsInstance(u, user_mgmt.User)
 
     def test_get_users(self):
-        user = users.Users(MagicMock())
-        user.client._ipf_pager.return_value = [{'id': '1108612054', 'isLocal': True, 'username': 'justin',
-                                                'ssoProvider': None, 'domainSuffixes': '',
-                                                'email': 'justin.jeffrey@ipfabric.io', 'customScope': True,
-                                                'scope': ['read', 'write', 'settings']}]
-        u = user.get_users('test')[0]
-        self.assertIsInstance(u, users.User)
+        u = self.usermgmt.get_users('test')[0]
+        self.assertIsInstance(u, user_mgmt.User)
 
     def test_get_user_by_id(self):
-        user = users.Users(MagicMock())
-        user.client._ipf_pager.return_value = [{'id': '1108612054', 'isLocal': True, 'username': 'justin',
-                                                'ssoProvider': None, 'domainSuffixes': '',
-                                                'email': 'justin.jeffrey@ipfabric.io', 'customScope': True,
-                                                'scope': ['read', 'write', 'settings']}]
-        u = user.get_user_by_id('1108612054')
-        self.assertIsInstance(u, users.User)
+        u = self.usermgmt.get_user_by_id('1108612054')
+        self.assertIsInstance(u, user_mgmt.User)
+
+    def test_add_user(self):
+        self.usermgmt.client.post().json.return_value = {'id': 1}
+        u = self.usermgmt.add_user('test', 'test', 'test1234', ['read'])
+        self.assertIsInstance(u, user_mgmt.User)
+
+    def test_add_user_fail(self):
+        with self.assertRaises(SyntaxError) as err:
+            self.usermgmt.add_user('test', 'test', 'test', ['read'])
+        with self.assertRaises(SyntaxError) as err:
+            self.usermgmt.add_user('test', 'test', 'test1234', ['hello'])
+
+    def test_delete_user(self):
+        u = self.usermgmt.delete_user('test1234')
+        self.assertIsInstance(u[0], user_mgmt.User)
