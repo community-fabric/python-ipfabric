9aca3fdb27a3f3a1a3dd4cbd4faad3417398048e
Justin Jeffery <justin.jeffery@ipfabric.io>
2021-12-08 08:31:16 -0500

    fix: Fix for unloaded snapshots Added users

--- ipfabric/settings/__init__.py
+++ ipfabric/settings/__init__.py
@@ -1,5 +1,6 @@
 from .api_tokens import APIToken
 from .authentication import Authentication
 from .seeds import Seeds
+from .users import Users
 
-__all__ = [Authentication, Seeds, APIToken]
+__all__ = [Authentication, Seeds, APIToken, Users]
