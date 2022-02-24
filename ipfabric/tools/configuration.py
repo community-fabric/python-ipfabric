bfa1722760d37b671fc2f8dc5a459eec5e8eb636
Justin Jeffery <justin.jeffery@ipfabric.io>
2022-02-24 14:21:13 -0500

    fix: log name.

--- ipfabric/tools/configuration.py
+++ ipfabric/tools/configuration.py
@@ -180,7 +180,7 @@ class DeviceConfigs:
         device = self._validate_device(device, snapshot_id=snapshot_id, log=True)
         if not device["sn"]:
             return None
-        return self.get_test_log(device)
+        return self.get_text_log(device)
 
     def get_text_log(self, device: dict):
         res = self.ipf.get("/os/logs/task/" + device["taskKey"])
