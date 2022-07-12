21015933ae6e01afefaa338ae722cc728d663720
Justin Jeffery <justin.jeffery@ipfabric.io>
2022-07-12 10:12:04 -0400

    Add back functional tests

--- tests/functional/test_client.py
+++ tests/functional/test_client.py
@@ -7,7 +7,6 @@ condition = False if os.getenv("IPF_TOKEN", None) and os.getenv("IPF_URL", None)
 
 
 @unittest.skipIf(condition, "IPF_URL and IPF_TOKEN not set")
-# @unittest.skipIf(True, "Skip till 5.0")  # TODO: Remove
 class MyTestCase(unittest.TestCase):
     def test_client(self):
         ipf = IPFClient()
