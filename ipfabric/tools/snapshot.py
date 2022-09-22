25ecb156819ba9999fe583d29b0cef2b7cd844be
Justin Jeffery <justin.jeffery@ipfabric.io>
2022-09-22 16:25:10 -0400

    Final Upload

--- ipfabric/tools/snapshot.py
+++ ipfabric/tools/snapshot.py
@@ -3,11 +3,8 @@ from pathlib import Path
 from ipfabric import IPFClient
 
 
-def upload(ipf: IPFClient, file: str, snapshot_name: str = None):
-    snapshot_name = snapshot_name or Path(file).stem
-    file = [
-        ('file', (snapshot_name, open(file, 'rb'), 'application/x-tar'))
-    ]
+def upload(ipf: IPFClient, file: str):
+    file = {'file': (Path(file).name, open(file, 'rb'), 'application/x-tar')}
     resp = ipf.post('snapshots/upload', headers={"Content-Type": "multipart/form-data"}, data=dict(), files=file)
     resp.raise_for_status()
     return resp.json()
