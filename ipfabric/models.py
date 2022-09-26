f74ff5fe25f892fdf783eac48deba1ef1636be02
Cristian Cordero <cristian.cordero@protonmail.com>
2022-09-26 13:09:10 -0400

    todos once httpx support

--- ipfabric/models.py
+++ ipfabric/models.py
@@ -5,7 +5,8 @@ from typing import Optional, Any, List
 from pydantic import BaseModel, Field
 
 from ipfabric.technology import *
-from ipfabric.tools.snapshot import download
+# todo: update snapshot obj with download method once httpx bug is fixed
+# from ipfabric.tools.snapshot import download
 
 logger = logging.getLogger("python-ipfabric")
 
@@ -83,8 +84,9 @@ class Snapshot(BaseModel):
         """
         return ipf.fetch_all("tables/snapshot-attributes", snapshot_id=self.snapshot_id)
 
-    def download(self, ipf, path):
-        return download(ipf,snapshot_id=self.snapshot_id, path=path)
+    # todo: uncomment once httpx supports form data file uploads
+    # def download(self, ipf, path):
+    #     return download(ipf,snapshot_id=self.snapshot_id, path=path)
 
 class Table(BaseModel):
     endpoint: str
