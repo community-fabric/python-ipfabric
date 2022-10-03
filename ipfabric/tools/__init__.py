814f5518e3dda483649897ea5fc376d7d85c398d
Justin Jeffery <justin.jeffery@ipfabric.io>
2022-10-03 13:57:38 -0400

    Added Discovery History Tools

--- ipfabric/tools/__init__.py
+++ ipfabric/tools/__init__.py
@@ -1,6 +1,7 @@
 from .configuration import DeviceConfigs
+from .discovery import DiscoveryHistory
 from .restore_intents import RestoreIntents
 from .sites import UpdateSiteNames
 from .vulnerabilities import Vulnerabilities
 
-__all__ = ["DeviceConfigs", "UpdateSiteNames", "Vulnerabilities", "RestoreIntents"]
+__all__ = ["DeviceConfigs", "UpdateSiteNames", "Vulnerabilities", "RestoreIntents", "DiscoveryHistory"]
