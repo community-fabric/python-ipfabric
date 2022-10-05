bc4ea9715e03ac96ca578186e32eeb28a26fcdd2
Justin Jeffery <justin.jeffery@ipfabric.io>
2022-10-05 12:56:25 -0400

    SA-218 Site Separation Name change was removed from IPF

--- ipfabric/tools/__init__.py
+++ ipfabric/tools/__init__.py
@@ -1,7 +1,6 @@
 from .configuration import DeviceConfigs
 from .discovery import DiscoveryHistory
 from .restore_intents import RestoreIntents
-from .sites import UpdateSiteNames
 from .vulnerabilities import Vulnerabilities
 
-__all__ = ["DeviceConfigs", "UpdateSiteNames", "Vulnerabilities", "RestoreIntents", "DiscoveryHistory"]
+__all__ = ["DeviceConfigs", "Vulnerabilities", "RestoreIntents", "DiscoveryHistory"]
