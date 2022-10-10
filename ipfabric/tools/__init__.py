20f9af42b43421cc72bd1e730fc767226bc4e725
Justin Jeffery <justin.jeffery@ipfabric.io>
2022-10-10 10:15:11 -0400

    Pull develop

--- ipfabric/tools/__init__.py
+++ ipfabric/tools/__init__.py
@@ -1,10 +1,9 @@
 from .configuration import DeviceConfigs
 from .discovery import DiscoveryHistory
 from .restore_intents import RestoreIntents
-from .sites import UpdateSiteNames
-from .snapshot import upload, download
 from .site_seperation_report import map_devices_to_rules
+from .snapshot import upload, download
 from .vulnerabilities import Vulnerabilities
 
-__all__ = ["DeviceConfigs", "Vulnerabilities", "RestoreIntents", "DiscoveryHistory", "map_devices_to_rules"]
-__all__ = ["DeviceConfigs", "UpdateSiteNames", "Vulnerabilities", "RestoreIntents", "upload", "download"]
+__all__ = ["DeviceConfigs", "Vulnerabilities", "RestoreIntents", "DiscoveryHistory", "map_devices_to_rules", "upload",
+           "download"]
