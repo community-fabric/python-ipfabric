2250f9fc3b18ecb61e84f8aecbf3642b49dfbbfb
Justin Jeffery <justin.jeffery@ipfabric.io>
2022-11-30 15:48:36 -0500

    Cleaned up _check_tech_table_attr

--- ipfabric/tools/table_diff.py
+++ ipfabric/tools/table_diff.py
@@ -109,27 +109,22 @@ class IpfTableDiff(BaseModel):
             IPFClient attribute for location of table data to be used in diff
         """
         valid_tables = [method for method in dir(self.client.technology) if method not in TECH_TABLE_METHODS_IGNORE]
-        if isinstance(tech_table, (tuple, str)):
-            if isinstance(tech_table, str):
-                tech_table = tuple(tech_table.split("."))
-            if isinstance(tech_table, tuple) and len(tech_table) != 2:
-                logger.error(f"unknown table {tech_table}")
-                logger.info(f"valid tables: \n {valid_tables}")
-                return None
-            first_attr = tech_table[0]
-            second_attr = tech_table[1]
-            tech_table_instance = getattr(self.client.technology, f"{first_attr}", None)
-            if tech_table_instance:
-                return_client = getattr(tech_table_instance, f"{second_attr}", None)
-                if not return_client:
-                    valid_tables = [method for method in dir(return_client) if method not in TECH_TABLE_METHODS_IGNORE]
-                    logger.error(f"unable to determine nested table {tech_table}")
-                    logger.info(f"valid tables: \n {valid_tables}")
-                return return_client
-        else:
-            logger.error(f"unable to determine nested table {tech_table}")
-            logger.info(f"valid tables: \n {valid_tables}")
+        if isinstance(tech_table, str):
+            tech_table = tuple(tech_table.split("."))
+        if not isinstance(tech_table, tuple) or len(tech_table) != 2:
             logger.info(f"{HELP_STR}")
+            return None
+        first_attr, second_attr = tech_table
+        tech_table_instance = getattr(self.client.technology, f"{first_attr}", None)
+        if not tech_table_instance:
+            logger.error(f"Unknown table {first_attr}\nValid tables:\n{valid_tables}")
+            return None
+        return_client = getattr(tech_table_instance, f"{second_attr}", None)
+        if not return_client:
+            valid_tables = [method for method in dir(return_client) if method not in TECH_TABLE_METHODS_IGNORE]
+            logger.error(f"Unable to determine nested table {second_attr}\nValid tables:\n{valid_tables}")
+            return None
+        return return_client
 
     def _check_inv_table_attr(self, inv_table: str):
         if hasattr(self.client.inventory, f"{inv_table}"):
@@ -143,10 +138,7 @@ class IpfTableDiff(BaseModel):
         technology: bool = False,
         technology_table: [str, tuple] = None
     ):
-        if technology:
-            table_data = self._check_tech_table_attr(technology_table)
-        else:
-            table_data = self._check_inv_table_attr(inv_table)
+        table_data = self._check_tech_table_attr(technology_table if technology else inv_table)
         snap_id = self.client.get_snapshot_id(snapshot)
         snap_id_compare = self.client.get_snapshot_id(snapshot_compare)
         return jd.diff(table_data.all(snapshot_id=snap_id), table_data.all(snapshot_id=snap_id_compare))
