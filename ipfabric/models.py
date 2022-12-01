3c2696b3f336f08b9b04043392a1f2cf90c8f56c
Cristian Cordero <cristian.cordero@protonmail.com>
2022-12-01 11:59:40 -0500

    adding inv tables
    was missing eof details and summary
    added osver-consistency

--- ipfabric/models.py
+++ ipfabric/models.py
@@ -125,6 +125,18 @@ class Inventory(BaseModel):
         return Table(client=self.client, endpoint="/tables/inventory/summary/models")
 
     @property
+    def os_version_consistency(self):
+        return Table(client=self.client, endpoint="tables/management/osver-consistency")
+
+    @property
+    def eof_summary(self):
+        return Table(client=self.client, endpoint="tables/reports/eof/summary")
+
+    @property
+    def eof_details(self):
+        return Table(client=self.client, endpoint="tables/reports/eof/detail")
+
+    @property
     def platforms(self):
         return Table(client=self.client, endpoint="/tables/inventory/summary/platforms")
 
